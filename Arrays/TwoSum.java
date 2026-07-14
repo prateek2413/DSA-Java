77cc93ca0621d12d41c230a68b77662da5b94da9
Prateek Bishnoi <prateekbishnoi24@gmail.com>
2026-07-14 22:20:22 +0530

    Clarify comment on optimization using hashmap
    Updated comment to clarify the need for a hashmap for an optimal solution.

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,4 +1,4 @@
-//this solution is not optimize use hashmap for optimize solution
+//this solution is not optimize use hashmap for optimize solution use hashmap for optimal solution
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
